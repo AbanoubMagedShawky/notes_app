ba15987aa5a6add476eb25c3c39c79e21d875d80
Abanoub Maged <abanoubmaged208@gmail.com>
2024-11-10 17:31:34 +0200

    create custom text field

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/add_note_botton_sheet.dart';
+import 'package:notes_app/widgets/constants.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -9,6 +10,7 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        backgroundColor: kPrimaryColor,
         onPressed: () {
           showModalBottomSheet(
               shape: RoundedRectangleBorder(
@@ -20,6 +22,7 @@ class NotesView extends StatelessWidget {
         },
         child: const Icon(
           Icons.add,
+          color: Colors.black,
         ),
       ),
       body: const NotesViewBody(),
