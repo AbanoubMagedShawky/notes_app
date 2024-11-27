47b6604241d1586fc2aead156d10162bb59e4341
Abanoub Maged <abanoubmaged208@gmail.com>
2024-11-27 14:52:02 +0200

    take input and validate date

--- lib/widgets/custom_botton.dart
+++ lib/widgets/custom_botton.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/constants.dart';
 
 class CustomBotton extends StatelessWidget {
-  const CustomBotton({super.key});
+  const CustomBotton({super.key, this.onTap});
 
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-              fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+    return GestureDetector(
+       onTap: onTap,
+      child: Container(
+        height: 50,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+                fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hintText, this.maxline = 1});
+  const CustomTextField(
+      {super.key, required this.hintText, this.maxline = 1, this.onsaved});
 
   final String hintText;
   final int maxline;
+
+  final void Function(String?)? onsaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onsaved,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Field is required';
+        }
+        return null;
+      },
       maxLines: maxline,
       textAlign: TextAlign.left,
       cursorColor: kPrimaryColor,
